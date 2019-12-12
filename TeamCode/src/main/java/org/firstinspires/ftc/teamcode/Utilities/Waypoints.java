99744e5818ed02c70b75080470f0d9ef17d1314f
Christian Esneul <nawaplayz@gmail.com>
2019-12-12 16:58:30 -0600

    Tweaked waypoints

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utilities/Waypoints.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utilities/Waypoints.java
@@ -174,11 +174,11 @@ public class Waypoints {
         blueLoading.put(LocationLoading.GRAB_SKYSTONE_B, new Navigation2D(blueStonePickupLocations.get(skystoneDetectionPosition + 3).x, blueStonePickupLocations.get(skystoneDetectionPosition + 3).y - 7, degreesToRadians((-90))));
         blueLoading.put(LocationLoading.ALIGNMENT_POSITION_B, blueStoneAlignmentLocations.get(skystoneDetectionPosition + 3));
         blueLoading.put(LocationLoading.FOUNDATION_ALIGNMENT, new Navigation2D(45, blueStoneAlignmentLocations.get(0).y, degreesToRadians(-90)));
-        blueLoading.put(LocationLoading.FOUNDATION_DROP_OFF, new Navigation2D(45, 26.74 + 1.75, degreesToRadians(-90)));
-        blueLoading.put(LocationLoading.BRIDGE_ALIGNMENT_OUTER, new Navigation2D(0, 0, degreesToRadians(-90)));
-        blueLoading.put(LocationLoading.BRIDGE_ALIGNMENT_INNER, new Navigation2D(0, 38.35, degreesToRadians(-90)));
+        blueLoading.put(LocationLoading.FOUNDATION_DROP_OFF, new Navigation2D(45, 26.74 + 1.75 - 4, degreesToRadians(-90)));
+        blueLoading.put(LocationLoading.BRIDGE_ALIGNMENT_OUTER, new Navigation2D(0, halfField - tileTabs - robotSidePadding, degreesToRadians(-90)));
+        blueLoading.put(LocationLoading.BRIDGE_ALIGNMENT_INNER, new Navigation2D(0, innerTileAlignment_Y, degreesToRadians(-90)));
         blueLoading.put(LocationLoading.PARK_OUTER, new Navigation2D(0, halfField - robotBackPadding, degreesToRadians(-90)));
-        blueLoading.put(LocationLoading.PARK_INNER, new Navigation2D(0, halfField - 1.2 * tileBody + robotFrontPadding, degreesToRadians(-90)));
+        blueLoading.put(LocationLoading.PARK_INNER, new Navigation2D(0, halfField - 1.7 * tileBody + robotFrontPadding, degreesToRadians(-90)));
         blueLoading.put(LocationLoading.SIMPLE_ALIGNMENT_INNER, new Navigation2D(-tileBody - robotSidePadding, halfField - 1.2 * tileBody + robotFrontPadding, degreesToRadians(-90)));
 
         /**
