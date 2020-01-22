4e33e885005093dd607530dbb3cdd8f6d00d5638
Christian Esneul <nawaplayz@gmail.com>
2020-01-21 18:06:51 -0600

    (Broken) Base for position controller

---
+++ TeamCode/src/test/java/PositionControllerTest.java
@@ -0,0 +1,51 @@
+import org.firstinspires.ftc.teamcode.Utilities.Constants;
+import org.firstinspires.ftc.teamcode.Utilities.MecanumNavigation;
+import org.firstinspires.ftc.teamcode.Utilities.PositionController;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import FakeHardware.FakeRobotHardware;
+
+public class PositionControllerTest {
+
+    FakeRobotHardware opMode = new FakeRobotHardware();
+    MecanumNavigation mecanumNavigation;
+    PositionController drive;
+    boolean arrived = false;
+    ScheduledExecutorService scheduler;
+    double simTime = 0;
+
+    @Before
+    public void init() {
+        opMode.initializeFakeOpMode();
+        mecanumNavigation = new MecanumNavigation(opMode, Constants.getDriveTrainMecanum());
+        mecanumNavigation.initialize(new MecanumNavigation.Navigation2D(0,0,0));
+        drive = new PositionController(opMode, mecanumNavigation);
+
+        scheduler = Executors.newScheduledThreadPool(1);
+        scheduler.scheduleAtFixedRate(new Thread(() -> {
+            System.out.println("Arrived: " + arrived
+                    + "\nX: " + mecanumNavigation.currentPosition.x
+                    + "\nY: " + mecanumNavigation.currentPosition.y
+                    + "\nTheta: " + mecanumNavigation.currentPosition.theta
+                    + "\nOuput: " );
+        }), 2, 2, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void run() {
+        double stepTime = 0.02;
+        // Keep thread running
+        while (true) {
+            MecanumNavigation.Navigation2D targetPosition = new MecanumNavigation.Navigation2D(10, 0, Math.toRadians(90));
+            arrived = drive.driveTo(targetPosition, 1);
+            opMode.updateAndIntegrateFakeOpMode(simTime);
+            mecanumNavigation.update();
+            simTime += stepTime;
+        }
+    }
+}
